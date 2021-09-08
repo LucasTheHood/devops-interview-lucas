47e8fe4cfdba5c58f32188c24705dbedc5a47c38
LucasTheHood <45661964+LucasTheHood@users.noreply.github.com>
2021-09-08 12:30:04 +0100

    Updated bucket-list.py
    If statement to check http header responses for bucket region from response of s3.head_bucket function

--- python/bucket-list.py
+++ python/bucket-list.py
@@ -1,6 +1,7 @@
 import boto3
 
-s3 = boto3.client('s3')
-buckets = s3.list_buckets()["Buckets"]
-for bucket in buckets:
-    print(bucket['Name'])
+s3 = boto3.client("s3")
+
+for bucket in s3.list_buckets()["Buckets"]:
+    if s3.head_bucket(Bucket=bucket['Name'])['ResponseMetadata']['HTTPHeaders']['x-amz-bucket-region'] == 'us-east-1':
+        print(bucket["Name"])
